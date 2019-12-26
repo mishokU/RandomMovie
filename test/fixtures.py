3da389800657a317e2dbeb7f7cffff28172c50e5
vlad <gilbert.cook111@gmail.com>
2019-12-26 23:52:45 +0300

    files and templates for ui tests added

---
+++ test/fixtures.py
@@ -0,0 +1,28 @@
+import pytest
+from appium import webdriver
+import constants as cnst
+
+desired_caps = {
+  "deviceName": "emulator-5554",
+  "platformName": "Android",
+  "appPackage": "com.sredasolutions.hmi.debug",
+  "appActivity": "com.sredasolutions.hmi.ui.activities.StatusActivity",
+  "noReset": True
+}
+
+
+@pytest.fixture(scope="session")
+def setup():
+    # test = Test()
+    # test.set_message("")
+    # test.start_test()
+    #
+    # yield test
+    assert 1 == 1
+
+
+@pytest.fixture(scope="session")
+def driver():
+    driver = webdriver.Remote(cnst.WEBDRIVER_LOCALHOST, desired_caps)
+
+    yield driver
