2ede34be52e9ed3b4d5a4a796b66959f5aee4ed4
vlad <gilbert.cook111@gmail.com>
2019-12-27 20:32:36 +0300

    fixtures have been updated (unused fixtures deleted)

--- test/fixtures.py
+++ test/fixtures.py
@@ -5,23 +5,10 @@ import constants as cnst
 desired_caps = {
   "deviceName": "emulator-5554",
   "platformName": "Android",
-  # "appPackage": "com.sredasolutions.hmi.debug",
-  # "appActivity": "com.sredasolutions.hmi.ui.activities.StatusActivity",
   "appPackage": cnst.APP_PACKAGE,
   "appActivity": cnst.APP_ACTIVITY,
   "noReset": True
 }
-
-
-@pytest.fixture(scope="session")
-def setup():
-    # test = Test()
-    # test.set_message("")
-    # test.start_test()
-    #
-    # yield test
-    assert 1 == 1
-
 
 @pytest.fixture(scope="session")
 def driver():
